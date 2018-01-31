6ac8158e52b8788be65c88566c212b865c0fa84a
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-01-31 13:23:33 -0700

    Step 3

--- app/app.js
+++ app/app.js
@@ -1,8 +1,32 @@
 angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider, $urlRouterProvider ) {
+  // .state can be chained
   $stateProvider
     .state('home',{
       url:'/',
       templateUrl: "app/about/aboutTmpl.html"
+    })
+    .state('packages',{
+      url:'/packages/:country',
+      templateUrl: "app/packages/packagesTmpl.html",
+      controller: 'packagesCtrl'
+    })
+    .state('locations',{
+      url:'/locations',
+      templateUrl: "app/locations/locationsTmpl.html",
+      controller: 'locationsCtrl'
+    })
+    .state('booked',{
+      url:'/booked/:id',
+      templateUrl: "app/booked/bookedTmpl.html",
+      controller: 'bookedCtrl'
+    })
+    .state('adventures',{
+      url:'/adventures',
+      templateUrl: "app/adventures/adventuresTmpl.html"
+    })
+    .state('contact',{
+      url:'/contact',
+      templateUrl: "app/contact/contactTmpl.html"
     });
 
   $urlRouterProvider
